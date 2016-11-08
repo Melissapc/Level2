c766a6df8a2e403ae1ad2132ba83657448f081d7
Leauge Mac <DONT CHANGE THIS!!!!!>
2016-11-07 17:14:58 -0800

    Level2
    mi nivel 2 cx

---
+++ src/BoxCollision.java
@@ -0,0 +1,34 @@
+import java.awt.Rectangle;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class BoxCollision {
+	JFrame frame;
+	final static int width = 600;
+	final static int height = 600;
+	GamePanel panel;
+	int x;
+	int y;
+	int rectwidth;
+	int rectheight;
+	Rectangle rectangle = new Rectangle();
+
+	public static void main(String[] args) {
+		BoxCollision box = new BoxCollision();
+		box.setup();
+	}
+
+	BoxCollision() {
+		frame = new JFrame();
+		panel = new GamePanel();
+
+	}
+
+	void setup() {
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.setSize(width, height);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
---
+++ src/GamePanel.java
@@ -0,0 +1,11 @@
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel{
+	
+	
+public void paintComponent(Graphics g){
+	g.fillRect(0, 0, 25, 25);
+}
+}
